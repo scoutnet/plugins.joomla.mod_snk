de233657fa84ad5876078d5de3bdcb25f2924c01
Stefan Horst <stefan@ultrachaos.de>
2009-09-21 17:18:32 +0000

    updated some template

--- mod_snk.php
+++ mod_snk.php
@@ -6,9 +6,13 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 //require_once( dirname(__FILE__).DS.'helper.php' );
 require_once(JPATH_SITE.DS.'components'.DS.'com_snk'.DS.'models'.DS.'snk.php');
  
-$helper = new SnkModelSnk();
-$events = $helper->getEvents($params);
-$kalenders = $helper->getKalenders($params);
+try {
+	$helper = new SnkModelSnk();
+	$events = $helper->getEvents($params);
+	$kalenders = $helper->getKalenders($params);
+	require( JModuleHelper::getLayoutPath( 'mod_snk' ) );
+} catch (Exception $e) {
+	echo "<span class='termin'>zZ ist der Scoutnet Kalender down.<br>Bitte versuch es zu einem sp&auml;teren Zeitpunkt noch mal</span>";
+}
 
-require( JModuleHelper::getLayoutPath( 'mod_snk' ) );
 ?>
--- tmpl/default.php
+++ tmpl/default.php
@@ -4,10 +4,12 @@ defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
 <?php JHTML::_('stylesheet', 'kalender.css', 'modules/mod_snk/assets/'); ?>
 
 
+<table>
 <? 
 foreach ($events as $event) { ?>
-<span class='snk_termin'>
-	<span class='snk_termin_date'><? echo strftime("%d.%m.%y",$event['Start']); ?></span>
-	<span class='snk_termin_text'><? echo $event['Title'];?></span>
-</span>
+<tr>
+	<td class='snk_termin_date'><? echo strftime("%d.%m.%y",$event['Start']); ?></td>
+	<td class='snk_termin_text'><? echo $event['Title'];?><br> second line</td>
+</tr>
 <?}?>
+</table>
